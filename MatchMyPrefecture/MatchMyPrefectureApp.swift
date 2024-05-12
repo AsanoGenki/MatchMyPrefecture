e683c9aa910d3d61a708e6689b4811a320a363cc
AsanoGenki <asanogenki0218@icloud.com>
2024-05-12 13:34:06 +0900

    Initial commit.

---
+++ MatchMyPrefecture/MatchMyPrefectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMyPrefectureApp.swift
+//  MatchMyPrefecture
+//
+//  Created by Genki on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MatchMyPrefectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
